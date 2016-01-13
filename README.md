3e755a47044ed20340f8bde875c945f9c5d036df
Shravan <shravan.kasagoni@outlook.com>
2016-01-13 06:22:11 +0530

    This where begin
    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Conference & Meeting Rooms Finder
+
+### Run The Example Using Below Commands:
+
+1. npm install
+2. npm start
---
+++ src/components/app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'room-finder',
+    templateUrl: 'src/components/app/app.html'
+})
+export class AppComponent {
+    public title = "Conference & Meeting Rooms Finder";
+}
